31c2683daa5f719994006eafab3a68f9c04cca61
yuma <yuuma_0117@icloud.com>
2025-09-17 21:59:48 +0900

    環境構築完了

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["http://localhost:3000"],
+      bodySizeLimit: "10mb",
+    },
+  },
 };
 
 export default nextConfig;
