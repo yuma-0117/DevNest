2ccc25348bd30265cf8065cacfb5395610ee5e73
yuma <yuuma_0117@icloud.com>
2025-09-20 18:57:27 +0900

    スレッド作成ページ完了

---
+++ src/app/thread/create/page.tsx
@@ -0,0 +1,22 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { fetchAllTagsAction } from "@/lib/actions/tag";
+import { ThreadCreateForm } from "./components/thread-create-form";
+
+const CreateThreadPage = async () => {
+  const allTags = await fetchAllTagsAction();
+
+  return (
+    <div className="container mx-auto py-8 flex justify-center">
+      <Card className="w-full max-w-2xl">
+        <CardHeader>
+          <CardTitle>Create a new thread</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ThreadCreateForm allTags={allTags} />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default CreateThreadPage;
