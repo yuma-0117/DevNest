6f9e30dd8d5f62aaf921681c0560000e593911d0
yuma <yuuma_0117@icloud.com>
2025-10-02 18:47:14 +0900

    refactor: update Prisma client instantiation pattern

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -1,7 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+declare global {
+  // allow global `var` declarations
+  var prisma: PrismaClient | undefined
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma = global.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prisma
+}
